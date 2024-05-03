bfd8aed23d056f4d3eca5264586f67238fee60e1
Harish0311 <harishgamer00@gmail.com>
2024-05-03 10:32:54 +0530

    basic testing done 👍

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  fixtures :users
+  def setup
+    @user = users(:one) # Load the fixture data for user 'one'
+    @user.password = "password"
+  end
   test "the truth" do
     assert true
   end
+
+  test "valid user should be valid" do
+    assert @user.valid?, @user.errors.full_messages.join(", ")
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.save
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = " "
+    assert_not @user.valid?
+  end
+
+  test "email should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+
+
+
 end
+
