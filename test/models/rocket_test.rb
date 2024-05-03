5bc15c17c65de0a2d85305018bc5b866550de32f
Harish0311 <harishgamer00@gmail.com>
2024-05-03 12:24:42 +0530

    testing the rocket model done 👍

--- test/models/rocket_test.rb
+++ test/models/rocket_test.rb
@@ -2,7 +2,24 @@ require "test_helper"
 
 class RocketTest < ActiveSupport::TestCase
 
+  fixtures :rockets
+
+  def setup
+    @rocket = rockets(:one)
+  end
+
   test "the truth" do
     assert true
   end
+
+  test "name required" do
+    @rocket.Name = ""
+    assert_not @rocket.save
+  end
+
+  test "Price less than zero" do
+    @rocket.Price = -1
+    assert_not @rocket.save
+  end
+
 end
