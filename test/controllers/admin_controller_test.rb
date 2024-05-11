5bfb74ed33df525a8ee0e834cb1d80eeda2f30c3
Harish0311 <harishgamer00@gmail.com>
2024-05-11 15:14:19 +0530

    Greatly improved code coverage for admin_controller_test.rb

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -16,4 +16,34 @@ class AdminControllerTest < ActionController::TestCase
     assert_redirected_to root_path
     assert_equal 'Rocket was successfully created.', flash[:notice]
   end
+
+
+  test 'should create a rocket model' do
+    sign_in
+
+    get :create
+    assert_response :success
+  end
+
+  test 'should not create a rocket ' do
+    sign_in
+    post :post_create, params: {
+      rocket: {
+      Name: 'Rocket Name',
+      Price: -1,
+      description: 'Rocket Description',
+      Image: 'rocket.jpg'
+      }
+    }
+
+    assert_equal "Rocket creation failed.", flash[:alert]
+  end
+
+  test 'should get edit' do
+    sign_in
+    rocket = rockets(:one)
+    get :edit, params: {id: rocket.id}
+    assert_response :success
+
+  end
 end
